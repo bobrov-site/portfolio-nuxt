3dcdff2e25bb6d2f9154395c7b2067d8e81b4009
Daniil Bobrov <allodrmen@yandex.ru>
2024-01-16 21:14:52 +0300

    installed bootstrap 5

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <h1>Привет мир</h1>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: ['~/assets/styles/main.scss'],
+  plugins: [
+    { src: '~/plugins/useBootstrap.ts', mode: 'client' }
+  ]
 })
