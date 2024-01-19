8502d5367d1603551952299ef8337a19ff3b9261
Daniil Bobrov <allodrmen@yandex.ru>
2024-01-19 10:42:29 +0300

    change credits path

--- components/Footer-bar.vue
+++ components/Footer-bar.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="bg-primary p-4 mt-4 text-center text-light">
         <p class="m-2">
-            <a class="text-link text-light" href="https://github.com/bobrov-site/portfolio" target="_blank">
+            <a class="text-link text-light" href="https://github.com/bobrov-site/portfolio-nuxt" target="_blank">
                 Исходный код сайта на GitHub
             </a>
         </p>
