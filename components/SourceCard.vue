c192e446cae9dba5866ac0318cbb288debd4d5b3
Daniil Bobrov <allodrmen@yandex.ru>
2024-01-29 10:12:18 +0300

    created sourceCard.vue

---
+++ components/SourceCard.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="card">
+        <div class="card-body">
+            <h3 class="card-title">{{ props.sectionTitle }}</h3>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    sectionTitle: String,
+    sources: Array,
+})
+</script>
+
+<style lang="scss" scoped></style>
