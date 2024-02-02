d3d226226541ccf4b881eb2b175bd72f06653753
Daniil Bobrov <allodrmen@yandex.ru>
2024-02-02 19:47:00 +0300

    added state for form in ModalForm.vue

--- components/ModalForm.vue
+++ components/ModalForm.vue
@@ -1,6 +1,31 @@
 <script setup>
+const title = ref()
+const description = ref()
+const category = ref()
+const url = ref()
+const pushedBy = ref()
+const isDisabled = ref(false);
+const errorsForm = ref({}) 
 const submitForm = () => {
-
+    isDisabled.value = true;
+    const formData = {
+        title: title.value,
+        description: description.value,
+        category: category.value,
+        url: url.value,
+        pushedBy: pushedBy.value
+    }
+    const validation = validateForm(formData)
+    validation
+    .then((response) => {
+        console.log(response);
+    })
+    .catch((e) => {
+        e.inner.forEach((error) => {
+            errorsForm.value[error.path] = error.message
+        });
+    })
+    isDisabled.value = false;
 }
 
 </script>
@@ -20,7 +45,8 @@ const submitForm = () => {
                     </p>
                     <form @submit.prevent="submitForm()">
                         <div class="mb-3">
-                            <select class="form-select" aria-label="Выберите категорию" required>
+                            <label for="category" class="form-label">Категория</label>
+                            <select v-model="category" class="form-select" aria-label="Выберите категорию" id="category" value="">
                                 <option selected>Статья</option>
                                 <option value="1">Репозиторий</option>
                                 <option value="2">Сайт</option>
@@ -29,27 +55,28 @@ const submitForm = () => {
                         </div>
                         <div class="mb-3">
                             <label for="title" class="form-label">Название</label>
-                            <input type="text" class="form-control" id="title" required>
+                            <input v-model="title" type="text" class="form-control" :class="{'is-invalid' : errorsForm.title}" id="title">
+                            <div v-if="errorsForm.title" class="invalid-feedback">{{ errorsForm.title }}</div>
                         </div>
                         <div class="mb-3">
                             <label for="description" class="form-label">Описание</label>
-                            <textarea class="form-control" id="description" rows="3"
+                            <textarea v-model="description" class="form-control" id="description" rows="3"
                                 aria-describedby="descriptionHelp"></textarea>
                             <div id="descriptionHelp" class="form-text">Необязательно</div>
                         </div>
                         <div class="mb-3">
                             <label for="url" class="form-label">Ссылка</label>
-                            <input type="text" class="form-control" id="url">
+                            <input v-model="url" type="text" class="form-control" id="url">
                         </div>
                         <div class="mb-3">
                             <label for="pushedBy" class="form-label">Ваше имя</label>
-                            <input type="text" class="form-control" id="pushedBy"
+                            <input v-model="pushedBy" type="text" class="form-control" id="pushedBy"
                                 aria-describedby="descriptionHelpPushedBy">
                             <div id="descriptionHelpPushedBy" class="form-text">Напишите ваше имя и оно будет опубликовано
                                 под каждым опубликованным вашим материалом, а так же внизу страницы в списке всех
                                 пользователей. Поле необязательное</div>
                         </div>
-                        <button type="submit" class="btn btn-primary">Опубликовать</button>
+                        <button type="submit" :disabled="isDisabled" class="btn btn-primary">Опубликовать</button>
                     </form>
                 </div>
             </div>
