310261133e7aa2c284810c36d386f185a2f2d6eb
Daniil Bobrov <allodrmen@yandex.ru>
2024-03-27 08:24:29 +0300

    remove supabase from config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  supabase: {
-    redirect: false,
-  },
   css: ['~/assets/styles/main.scss'],
   plugins: [
     { src: '~/plugins/useBootstrap.ts', mode: 'client' },
