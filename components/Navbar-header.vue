02751aad9c3f4d3d4271508f0d12c86c86098881
Daniil Bobrov <allodrmen@yandex.ru>
2024-05-23 19:28:46 +0300

    added opensource link

--- components/Navbar-header.vue
+++ components/Navbar-header.vue
@@ -135,6 +135,9 @@
                                 </li>
                             </ul>
                         </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="https://github.com/bobrov-site/contributed" target="_blank">Open-source</a>
+                        </li>
                     </ul>
                     <div class="d-flex">
                         <a class="link-icon" href="https://github.com/bobrov-site" target="_blank">
