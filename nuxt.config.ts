e86fa6fe5e1775b5486d84e225ca927105ac7afd
Daniil Bobrov <allodrmen@yandex.ru>
2024-06-30 22:41:02 +0300

    update nuxt seo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,12 @@ export default defineNuxtConfig({
     { src: '~/plugins/useBootstrap.ts', mode: 'client' },
   ],
   modules: ["nuxt-bootstrap-icons", "yandex-metrika-module-nuxt3"],
+  site: {
+    url: 'https://bobrov-site.vercel.app',
+    name: 'Бобров Даниил | Портфолио | Frontend-developer | Vue | Nuxt | React | JS | TS | GIT',
+    description: 'Занимаюсь разработкой сайтов 3+ года. Мною были разработаны разного типа: лендинги, многостраничные сайты и интернет магазины разной коммерческой направленности. В моей базе более 30 положительных отзывов от клиентов предпринимателей в малом и среднем бизнесе. Так же занимался разработкой сайтов под ключ (дизайн + посадка на Wordpress). На данный момент занимаюсь разработкой приложений на Vue.js',
+    defaultLocale: 'ru', // not needed if you have @nuxtjs/i18n installed
+  },
   yandexMetrika: {
     id: '97306384',
   },
