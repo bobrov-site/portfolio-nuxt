9dd441ea02579a8daad9a6da103bbbe8f1374609
Daniil Bobrov <allodrmen@yandex.ru>
2024-10-24 11:41:39 +0300

    update to github profile in footer

--- components/Footer-bar.vue
+++ components/Footer-bar.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="bg-primary p-4 mt-4 text-center text-light">
         <p class="m-2">
-            <a class="text-link text-light" href="https://github.com/bobrov-site/portfolio-nuxt" target="_blank">
-                Исходный код сайта на GitHub
+            <a class="text-link text-light" href="https://github.com/bobrov-site" target="_blank">
+                GITHUB
             </a>
         </p>
     </div>
