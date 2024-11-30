abadfaf49c30856a90427cea4bdd029bb2688854
Daniil Bobrov <allodrmen@yandex.ru>
2024-11-30 22:53:00 +0300

    added ajax project

---
+++ pages/sites/nuxt/Ajax.vue
@@ -0,0 +1,27 @@
+<template>
+    <ProjectContent :techStack="techStack" :siteUrl="siteUrl" :title="title"
+    :responsibilities="responsibilities" :achivments="achivments" />
+</template>
+
+<script setup>
+import ProjectContent from '../components/Project-content.vue'
+useSeoMeta({
+    title: title,
+    description: achivments,
+    ogDescription: achivments
+})
+const achivments = 'Выполнено более 30 задач технической поддержки интерфейса приложения на Nuxt.js <br>'
+const responsibilities = ['Исправление баг и создание новых фич технической поддержкой']
+
+const title = 'Аякс. Поиск недвижимости в Краснодаре'
+
+const siteUrl = 'https://www.ayax.ru/'
+const techStack = [
+    'HTML',
+    'CSS',
+    'js',
+    'Vue.js',
+    'Nuxt.js',
+    '$fetch',
+]
+</script>
