a5b2d992b713b7ec029a2baf63cec72c57055244
Daniil Bobrov <allodrmen@yandex.ru>
2024-12-01 10:41:27 +0300

    change footer

--- components/Footer-bar.vue
+++ components/Footer-bar.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="bg-primary p-4 mt-4 text-center text-light">
         <p class="m-2">
-            <a class="text-link text-light" href="https://github.com/bobrov-site" target="_blank">
-                GITHUB
-            </a>
+            <NuxtLink class="text-link text-light" to="https://github.com/bobrov-site/portfolio-nuxt" external target="_blank">
+                Исходный код сайта
+            </NuxtLink>
         </p>
     </div>
 </template>
